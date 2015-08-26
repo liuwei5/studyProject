7c12e2f61860060933f27e2929eb1f1787bb94e9
liuwei5 <jingzhouliuwei@126.com>
2015-08-26 18:01:16 +0800

    添加swift工程

---
+++ StudyProject/SwiftStudy/SwiftStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by BrotherWei on 15/8/26.
+//  Copyright (c) 2015年 paDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
